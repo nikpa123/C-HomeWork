07a248e90cf0f4c06ab3fe61dc2d35fa046efc32
Pavel <nikpa123@yandex.ru>
2022-11-18 16:10:55 +0300

    9 HW completed

---
+++ Seminar_9/Task_64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string NumbersRec(int n, int a = 1)
+{
+    if (a <= n)
+    {
+        return NumbersRec(n, a + 1) + a.ToString() + " ";
+    }
+    return String.Empty;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumbersRec(n));
---
+++ Seminar_9/Task_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbersRec(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return m + SumNumbersRec(m + 1, n);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumbersRec(m, n));
